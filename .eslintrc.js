cb6d9622f8ed9a11524d2062b020ea771d8a108f
Gerald <code@gera2ld.space>
2023-09-12 15:03:59 +0800

    chore: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,8 @@ module.exports = {
   root: true,
   extends: [
     require.resolve('@gera2ld/plaid/eslint'),
-    require.resolve('@gera2ld/plaid-common-ts/eslint'),
   ],
   parserOptions: {
     project: './tsconfig.json',
-  },
-  globals: {
-    VM: true,
-  },
-  rules: {
-    '@typescript-eslint/no-this-alias': 'off',
   },
 };
